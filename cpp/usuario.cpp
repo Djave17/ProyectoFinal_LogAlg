25e618e9bdd778b2262f0bcaed7132f5579c9d6a
soli.tr4 <164054168+soliethTrejos@users.noreply.github.com>
2024-06-20 21:13:56 -0600

    Update usuario.cpp

--- cpp/usuario.cpp
+++ cpp/usuario.cpp
@@ -3,8 +3,13 @@
 
 using namespace std;
 
-int main() {
-    char password[8]; 
+#define MIN 4
+#define MAX 16
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    char password[MAX + 1]; 
     int i = 0;
     cout << "Bienvenido a la plataforma de control de partidos de los jaguares UAM" << endl;
     cout << "Ingrese su usuario: ";
@@ -12,7 +17,7 @@ int main() {
     cin >> usuario;
     cout << "Usuario: " << usuario << endl;
     cout << "Ingrese su contraseña: ";
-    while ((password[i] = getch())!= '\r') { 
+    while ((password[i] = getch()) != '\r') { 
         if (password[i] == '\b') { 
             if (i > 0) {
                 i--; 
@@ -24,5 +29,19 @@ int main() {
         }
     }
     password[i] = '\0'; 
+
+    // Verificar longitud de contraseña
+    if (i < MIN) {
+        cout << "La contraseña debe tener al menos " << MIN << " caracteres." << endl;
+        return 1;
+    }
+
+    if (i > 0) {
+        cout << "Contraseña válida. Bienvenido!" << endl;
+    } else {
+        cout << "Contraseña inválida. Intente nuevamente." << endl;
+        return 1;
+    }
+
     return 0;
 }
